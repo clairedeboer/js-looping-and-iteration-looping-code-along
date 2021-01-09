36af95fe8e03cfb8da7d8862cac3241b377fda0b
clairedeboer <claire.deboer@gmail.com>
2021-01-09 18:23:45 -0500

    commit tree c61e159c5dd6a70be14f1d5e546fe4ce423dfb87

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const namesArray = ["Ada", "Brendan", "Ali"]; 
+function writeCards(namesArray, event) {
+  let thankYouCards = []
+  for (let i = 0; i < namesArray.length; i++) {
+  thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`); 
+  }
+  return thankYouCards; 
+}
+
+function countDown(positiveInteger) {
+  let i = positiveInteger; 
+  while (i >= 0) {
+    console.log(i); 
+    i--; 
+  }
+}
+
+
